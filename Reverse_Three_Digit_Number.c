ae3b8019bfcd5e6f8e2f8df51889f53e739c865a
gr4527 <35078477+gr4527@users.noreply.github.com>
2024-02-27 09:00:02 -0500

    Add files via upload

---
+++ Reverse_Three_Digit_Number.c
@@ -0,0 +1,19 @@
+// Reverse the digits of a three digit number
+#include <stdio.h>
+
+int main(void)
+{
+  int number, last_digit, middle_digit, first_digit; // declaring variables
+
+  printf("Enter a three-digit number: "); // prompt
+  scanf("%d", &number); // input
+
+  last_digit = number % 10; // gives the last digit
+  number /= 10;        // number = number / 10
+  middle_digit = number % 10;   // gives middle digit
+  first_digit = number / 10;    // gives first digit
+
+  printf("The reversal is: %d%d%d", last_digit, middle_digit, first_digit);
+
+  return 0;
+}
