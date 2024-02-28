37789b2ac59a62f4392bf1feb96ceb6c554d3067
gr4527 <35078477+gr4527@users.noreply.github.com>
2024-02-28 18:16:00 -0500

    Not the best way solving this considering safety

---
+++ Find_The_Largest_Of_Four_Integers.c
@@ -0,0 +1,48 @@
+// Find the largest of four integers. Repeating gives one of the variables which might not be security wise desired
+#include <stdio.h>
+
+int main(void)
+{
+  int i, j, k, m, largest;
+
+  printf("Enter four integers: ");
+  scanf("%d%d%d%d", &i, &j, &k, &m);
+
+// Without using sorting algorithm this is one of the ways solving this
+// Not Recommended solution because it is error prone
+  if(i > j)
+  {
+    if(i > k)
+    {
+      if(i > m)
+        largest = i;
+      else if(k > j)
+        if(k > m)
+          largest = k;
+        else
+          largest = m;
+    }
+  }
+  else if(j > k)
+  {
+    if(j > m)
+      largest = j;
+  }
+  else if(k > m)
+    largest = k;
+  else
+    largest = m;
+
+  printf("Largest is %d", largest);
+
+//**********************************
+  //Other possible solution idea
+  //else if(j > i && j > k && j > m)
+  //largest = j
+  //else
+  //k >
+  // i > j ? value : value
+//**********************************
+
+  return 0;
+}
